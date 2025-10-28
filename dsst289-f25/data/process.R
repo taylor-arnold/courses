8c10fb92576587f25f882e76045b8b4bf189fc4b
taylor-arnold <taylor@dvlab.org>
2025-10-28 14:22:10 -0400

    auto update

--- dsst289-f25/data/process.R
+++ dsst289-f25/data/process.R
@@ -7,7 +7,14 @@ f <- c(file.path("copy", f1), file.path("write", f2))
 task <- rep(c("copy", "write"), c(length(f1), length(f2)))
 
 df <- vector("list", length(f))
-for (i in seq_along(df)) {
+for (j in seq_along(df)) {
   x <- read_csv(f[j], na="NA")
-  x$id <- i
+  x$id <- j
+  x$task <- task[j]
+  x <- select(x, id, task, everything())
+  df[[j]] <- x
 }
+df <- bind_rows(df)
+df$key[df$key_code=="Space"] <- " "
+
+write_csv(df, "keylog_class.csv.bz2")
